51a3a3cc9d1c39399a54bda5d121a288d10145a6
tahmidefaz <tahmidefaz@gmail.com>
2021-08-17 19:55:32 -0400

    configuring for cloud run

--- run_api.py
+++ run_api.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Resource, Api
 
@@ -11,5 +13,6 @@ api.add_resource(Ping, '/ping', '/ping/')
 api.add_resource(Activity, '/activity', '/activity/')
 api.add_resource(Footprint, '/footprint', '/footprint/')
 
+port = int(os.environ.get('PORT', 5000))
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(threaded=True, debug=True, host='0.0.0.0', port=port)
